4ade055d47c91072523a3a7773a973721bb8b29c
Neo Anderson <neo5anderson@icloud.com>
2017-02-28 22:39:26 +0800

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# Cordova 应用模板
+
+## 初始化
+
+```sh
+cordova create <name> --template allview@172.16.248.4:0xAllview/cordova-allview-app-template.git <name>
+cd <name>
+npm install
+gulp
+```
+
+## 运行
+
+```sh
+cordova platform add browser
+cordova run browser
+```
+
---
+++ template_src/gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var bower = require('gulp-bower');
+
+gulp.task('bower', function () {
+    return bower('./bower');
+});
+
+gulp.task('default', ['bower'],function () {
+    gulp.src(['bower/dcloud-mui/dist/**/*'],
+            { base: 'bower/dcloud-mui/dist' })
+        .pipe(gulp.dest('www/vender'));  
+    gulp.src([ 'bower/echarts/dist/echarts.js',
+            'bower/echarts/dist/echarts.min.js'],
+            { base: 'bower/echarts/dist' })
+        .pipe(gulp.dest('www/vender/js'));  
+    gulp.src([ 'bower/jquery/dist/jquery.js',
+            'bower/jquery/dist/jquery.min.js'],
+            { base: 'bower/jquery/dist' })
+        .pipe(gulp.dest('www/vender/js'));  
+});
